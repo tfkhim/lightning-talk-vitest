14e2ebae14b5466b34a4da767a0a7b3efe8f9bbe
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2022-07-13 22:28:53 +0200

    Extend Vitest showcase

--- vitest/ApiShowcase.spec.ts
+++ vitest/ApiShowcase.spec.ts
@@ -1,6 +1,12 @@
 import { greet } from "../src/greeter"
 
+vi.mock('../src/greeter')
+
 describe('Showcase', () => {
+    afterEach(() => {
+        vi.useRealTimers()
+    })
+
     it('has the same expect syntax as Jest', () => {
         const greeting = greet()
 
@@ -11,5 +17,40 @@ describe('Showcase', () => {
         const greeting = greet()
 
         expect(greeting).toBeString()
+    })
+
+    it('has mocks for functions', () => {
+        const mock = vi.fn<[string], undefined>()
+
+        mock('Hello World!')
+
+        expect(mock).toHaveBeenCalledWith('Hello World!')
+    })
+
+    it('can spy on methods', () => {
+        const obj = {
+            method() {}
+        }
+        vi.spyOn(obj, 'method')
+
+        obj.method()
+
+        expect(obj.method).toHaveBeenCalledOnce()
+    })
+
+    it('can mock timers', async () => {
+        vi.useFakeTimers()
+
+        const sleep = new Promise((resolve) => setTimeout(resolve, 60000))
+
+        vi.advanceTimersByTime(61000)
+
+        await expect(sleep).toResolve()
+    })
+
+    it('can mock modules', async () => {
+        greet()
+
+        expect(greet).toHaveBeenCalledOnce()
     })
 })
