44a0e48046b68ffdcd24a8670c88dc06f6330db3
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2022-07-30 21:46:36 +0200

    Add documentation for some config variables

--- vitest/vite.config.ts
+++ vitest/vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    globals: true,
-    setupFiles: ["jest-extended/all"],
-    restoreMocks: true
+    globals: true,                     // Avoiding imports of describe, it and expect
+    setupFiles: ["jest-extended/all"], // Any setup you need in all test suites
+    includeSource: ['./index.ts'],     // Tell Vitest where In-Source tests are located
+    restoreMocks: true                 // Restore all mocks before each test
   },
 })
